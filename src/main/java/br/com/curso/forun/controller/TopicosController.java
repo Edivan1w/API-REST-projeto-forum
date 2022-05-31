597d0c06c88b04097c76c1bec9911f635c30542d
unknown <edwanestudos@gmail.com>
2022-05-31 14:02:07 -0300

    Uso de cache na aplicação

--- src/main/java/br/com/curso/forun/controller/TopicosController.java
+++ src/main/java/br/com/curso/forun/controller/TopicosController.java
@@ -11,6 +11,8 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -47,6 +49,7 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
+	@Cacheable(value = "listaDeTopicos")
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
 			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao){
 	
@@ -60,7 +63,8 @@ public class TopicosController {
 	}
 	//tem que ser devolvido o status 201
 	@PostMapping
-	@Transactional
+	@Transactional  //serve para dizer ao spring para lipar o cache.
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.coverter(cursoRepository);
 		topicoReposiry.save(topico);
@@ -82,6 +86,7 @@ public class TopicosController {
 
 	@PutMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
 		Optional<Topico> optional = topicoReposiry.findById(id);
 		if(optional.isPresent()) {
@@ -94,6 +99,7 @@ public class TopicosController {
 	}
 	
 	@DeleteMapping("/{id}")
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> deletar(@PathVariable Long id){
 		Optional<Topico> optional = topicoReposiry.findById(id);
 		if(optional.isPresent()) {
