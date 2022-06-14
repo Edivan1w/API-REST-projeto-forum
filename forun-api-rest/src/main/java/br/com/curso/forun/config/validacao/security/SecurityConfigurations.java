9edf3b3779d3c4f08e86ff82bf42654a674ed2d9
unknown <edwanestudos@gmail.com>
2022-06-14 16:40:45 -0300

    Adicionando monitoramento da api, utilizando actuator

--- forun-api-rest/src/main/java/br/com/curso/forun/config/validacao/security/SecurityConfigurations.java
+++ forun-api-rest/src/main/java/br/com/curso/forun/config/validacao/security/SecurityConfigurations.java
@@ -48,6 +48,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET ,"/topicos").permitAll()
 		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
+		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 		//qualquer outra requisição tem que estar autenticada
 		.anyRequest().authenticated()
 		.and().csrf().disable()
